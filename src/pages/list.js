e6043765e6ad5efc01b372686ea49ed4d90e7e4a
swain <iamswain@hotmail.com>
2019-06-17 17:15:36 -0700

    refactor title lang

--- src/pages/list.js
+++ src/pages/list.js
@@ -2,18 +2,17 @@ import React from "react";
 import Calendar from "../components/calendar";
 import SEO from "../components/seo";
 import dates from "./dates";
-/*
-
-es = 1994 Wang Ning <<Vida política>> Traducción automática
-fr = 1994 Wang Huning <<vie politique>> Traduction automatique
-ja = 1994年
-ms = Tahun 1994, Wang huning, kehidupan politik, terjemahan automatik
-ru = Год 1994, Wang huning, Политическая жизнь
-vi = Năm 1994, Wang huning, Đời sống chính trị, dịch tự động
-zh = 1994年
-ko = 1994년 왕후닝 <<정치적 인생>> 번역 및 자동번역
-EN = Year 1994, Wang huning, Political life, auto-translation
-*/
+const titleLang = {
+  es: "1994 Wang Ning <<Vida política>> Traducción automática",
+  fr: "1994 Wang Huning <<vie politique>> Traduction automatique",
+  ja: "1994年",
+  ms: "Tahun 1994, Wang huning, kehidupan politik, terjemahan automatik",
+  ru: "Год 1994, Wang huning, Политическая жизнь",
+  vi: "Năm 1994, Wang huning, Đời sống chính trị, dịch tự động",
+  zh: "1994年",
+  ko: "1994년 왕후닝 <<정치적 인생>> 번역 및 자동번역",
+  EN: "Year 1994, Wang huning, Political life, auto-translation"
+};
 
 export default props => {
   const { lang } = props.match.params;
@@ -21,37 +20,8 @@ export default props => {
     "Year 1994, Wang huning, Political life, auto-translation"
   );
   React.useEffect(() => {
-    switch (lang) {
-      case "es":
-        setTitle("1994 Wang Huning <<Vida política>> Traducción automática");
-        break;
-      case "fr":
-        setTitle("1994 Wang Huning <<vie politique>> Traduction automatique");
-        break;
-      case "ja":
-        setTitle("1994年");
-        break;
-      case "ms":
-        setTitle(
-          "Tahun 1994, Wang huning, kehidupan politik, terjemahan automatik"
-        );
-        break;
-      case "ru":
-        setTitle("Год 1994, Wang huning, Политическая жизнь");
-        break;
-      case "vi":
-        setTitle("Năm 1994, Wang huning, Đời sống chính trị, dịch tự động");
-        break;
-      case "zh":
-        setTitle("1994年");
-        break;
-      case "ko":
-        setTitle("1994년 왕후닝 <<정치적 인생>> 번역 및 자동번역");
-        break;
-      case "en":
-        setTitle("Year 1994, Wang huning, Political life, auto-translation");
-        break;
-      default:
+    if (titleLang[lang]) {
+      setTitle(titleLang[lang]);
     }
   }, [lang]);
   return (
